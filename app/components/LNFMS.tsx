dea8ad71c0f846d8f85c848a97bb1db98bbdc598
charlie-1126 <charlie869599@gmail.com>
2025-03-17 17:31:33 +0900

    feat: LNFMS pagination

--- app/components/LNFMS.tsx
+++ app/components/LNFMS.tsx
@@ -2,6 +2,15 @@ import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 import { LNFMSCard } from './LNFMSCard';
 import { Input } from './ui/input';
 import { Label } from './ui/label';
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from './ui/pagination';
 import { ArrowLeft, LogOut, Search, Trash2 } from 'lucide-react';
 import { LuSquareCheckBig, LuPlus } from 'react-icons/lu';
 import { FaCheck } from 'react-icons/fa6';
@@ -42,6 +51,10 @@ export function LNFMS({ items }: { items: (typeof itemTable.$inferSelect)[] }) {
   const [receiverName, setReceiverName] = React.useState<string | null>(null);
   //이미지 팝업
   const [selectedImage, setSelectedImage] = React.useState<string | null>(null);
+
+  //페이지
+  const [currentPage, setCurrentPage] = React.useState<number>(1);
+  const itemsPerPage = 10;
 
   const onImageClick = (item: typeof itemTable.$inferSelect) => {
     setSelectedImage(item.image?.length ? item.image : '/image/noImg.gif');
@@ -146,6 +159,41 @@ export function LNFMS({ items }: { items: (typeof itemTable.$inferSelect)[] }) {
       return true;
     });
   }, [itemsList, filters]);
+  const totalPages = Math.ceil(items.length / itemsPerPage);
+
+  // 단일 삭제
+  const deleteItem = () => {
+    if (deleteIndex !== null) {
+      setItemsList(itemsList.filter((_, i) => i !== deleteIndex));
+      setDeleteIndex(null);
+    }
+    setDeleteDialogOpen(false);
+  };
+
+  // 단일 반환
+  const returnItem = () => {
+    if (returnIndex !== null) {
+      setItemsList(itemsList.filter((_, i) => i !== returnIndex));
+      setReturnIndex(null);
+    }
+    setReturnDialogOpen(false);
+  };
+
+  // 다중 삭제 처리
+  const multipleDeleteItem = () => {
+    setItemsList(itemsList.filter((_, i) => !selectList.includes(i)));
+    setSelectList([]);
+    setMultipleSelection(false);
+    setMultipleDeleteDialogOpen(false);
+  };
+
+  // 다중 반환 처리
+  const multipleReturnItem = () => {
+    setItemsList(itemsList.filter((_, i) => !selectList.includes(i)));
+    setSelectList([]);
+    setMultipleSelection(false);
+    setMultipleReturnDialogOpen(false);
+  };
 
   // 카드클릭 로직
   const cardClick = (index: number) => {
@@ -161,115 +209,201 @@ export function LNFMS({ items }: { items: (typeof itemTable.$inferSelect)[] }) {
   };
 
   return (
-    <div className='flex h-screen flex-col p-2 md:p-4'>
-      <Card className='flex h-full flex-col gap-4'>
-        <CardHeader className='flex flex-row items-center justify-between space-y-0 px-2.5 pr-5 md:px-6'>
-          <div className='flex items-center gap-2'>
-            {/* 뒤로가기 버튼 */}
-            <Link
-              to='/'
-              className='hover:bg-muted cursor-pointer rounded-full p-2 transition-colors'
-            >
-              <ArrowLeft size={20} />
-            </Link>
-            <CardTitle className='text-2xl'>LNFMS</CardTitle>
-          </div>
-          <div className='flex items-center gap-4'>
-            {/* 다중선택시 나오는 구역 */}
-            <div
-              className={`flex gap-0 transition-all duration-300 ease-out md:gap-2 ${
-                multipleSelection
-                  ? 'translate-x-3 opacity-100'
-                  : 'pointer-events-none translate-x-10 opacity-0'
-              }`}
-            >
-              {/* 다중 반환 버튼 */}
+    <div className='flex h-screen flex-col p-2 md:p-4 md:pb-2'>
+      <div className='flex-1 overflow-y-auto'>
+        <Card className='flex h-full flex-col gap-4 pb-0'>
+          <CardHeader className='flex flex-row items-center justify-between space-y-0 px-2.5 pr-5 md:px-6'>
+            <div className='flex items-center gap-2'>
+              {/* 뒤로가기 버튼 */}
+              <Link
+                to='/'
+                className='hover:bg-muted cursor-pointer rounded-full p-2 transition-colors'
+              >
+                <ArrowLeft size={20} />
+              </Link>
+              <CardTitle className='text-2xl'>LNFMS</CardTitle>
+            </div>
+            <div className='flex items-center gap-4'>
+              {/* 다중선택시 나오는 구역 */}
+              <div
+                className={`flex gap-0 transition-all duration-300 ease-out md:gap-2 ${
+                  multipleSelection
+                    ? 'translate-x-3 opacity-100'
+                    : 'pointer-events-none translate-x-10 opacity-0'
+                }`}
+              >
+                {/* 다중 반환 버튼 */}
+                <button
+                  onClick={() => {
+                    setMultipleReturnDialogOpen(true);
+                  }}
+                  className='cursor-pointer rounded-full p-1.5 text-green-400 transition-colors hover:bg-green-100'
+                  aria-label='다중반환'
+                >
+                  <FaCheck size={20} />
+                </button>
+                {/* 다중 삭제 버튼 */}
+                <button
+                  onClick={() => setMultipleDeleteDialogOpen(true)}
+                  className='cursor-pointer rounded-full p-1.5 text-red-500 transition-colors hover:bg-red-100'
+                  aria-label='다중삭제'
+                >
+                  <Trash2 size={20} />
+                </button>
+              </div>
+              {/* 다중 선택 버튼 */}
               <button
                 onClick={() => {
-                  setMultipleReturnDialogOpen(true);
+                  if (multipleSelection) {
+                    setSelectList([]);
+                  }
+                  setMultipleSelection(!multipleSelection);
                 }}
-                className='cursor-pointer rounded-full p-1.5 text-green-400 transition-colors hover:bg-green-100'
-                aria-label='다중반환'
+                className={`flex cursor-pointer items-center gap-1 rounded-full p-1.5 text-sm text-black transition-colors ${
+                  multipleSelection ? 'bg-blue-100' : 'hover:bg-gray-100'
+                }`}
               >
-                <FaCheck size={20} />
+                <LuSquareCheckBig size={18} />
               </button>
-              {/* 다중 삭제 버튼 */}
-              <button
-                onClick={() => setMultipleDeleteDialogOpen(true)}
-                className='cursor-pointer rounded-full p-1.5 text-red-500 transition-colors hover:bg-red-100'
-                aria-label='다중삭제'
+
+              {/* 로그아웃 버튼 */}
+              <Link
+                to='/logout'
+                className='text-muted-foreground hover:text-foreground flex cursor-pointer items-center gap-1 text-sm transition-colors'
               >
-                <Trash2 size={20} />
-              </button>
+                <LogOut size={18} />
+                Logout
+              </Link>
             </div>
-            {/* 다중 선택 버튼 */}
-            <button
-              onClick={() => {
-                if (multipleSelection) {
-                  setSelectList([]);
-                }
-                setMultipleSelection(!multipleSelection);
-              }}
-              className={`flex cursor-pointer items-center gap-1 rounded-full p-1.5 text-sm text-black transition-colors ${
-                multipleSelection ? 'bg-blue-100' : 'hover:bg-gray-100'
-              }`}
-            >
-              <LuSquareCheckBig size={18} />
-            </button>
+          </CardHeader>
 
-            {/* 로그아웃 버튼 */}
-            <Link
-              to='/logout'
-              className='text-muted-foreground hover:text-foreground flex cursor-pointer items-center gap-1 text-sm transition-colors'
-            >
-              <LogOut size={18} />
-              Logout
-            </Link>
-          </div>
-        </CardHeader>
+          {/* 필터 섹션 */}
+          <CardContent className='px-2 py-0 md:px-6'>
+            <FilterSection filters={filters} setFilters={setFilters} resetFilters={resetFilters} />
+          </CardContent>
 
-        {/* 필터 섹션 */}
-        <CardContent className='px-2 py-0 md:px-6'>
-          <FilterSection filters={filters} setFilters={setFilters} resetFilters={resetFilters} />
-        </CardContent>
-
-        {/* 분실물 카드 */}
-        <CardContent className='flex-1 space-y-4 overflow-y-auto px-2 pb-4 md:px-6'>
-          {filteredItems.length > 0 ? (
-            filteredItems.map((item, index) => (
-              <LNFMSCard
-                onClick={() => cardClick(index)}
-                key={index}
-                item={item}
-                onReturn={() => {
-                  setReturnIndex(index);
-                  setReturnDialogOpen(true);
-                }}
-                onDelete={() => {
-                  setDeleteIndex(index);
-                  setDeleteDialogOpen(true);
-                }}
-                onImageClick={() => {
-                  onImageClick(item);
-                }}
-                className={`cursor-pointer transition-colors ${selectList.includes(index) ? 'bg-blue-50' : ''}`}
-                multiple={multipleSelection}
+          {/* 분실물 카드 */}
+          <CardContent className='flex-1 space-y-4 overflow-y-auto px-2 pb-4 md:px-6'>
+            {filteredItems.length > 0 ? (
+              filteredItems.map((item, index) => (
+                <LNFMSCard
+                  onClick={() => cardClick(index)}
+                  key={index}
+                  item={item}
+                  onReturn={() => {
+                    setReturnIndex(index);
+                    setReturnDialogOpen(true);
+                  }}
+                  onDelete={() => {
+                    setDeleteIndex(index);
+                    setDeleteDialogOpen(true);
+                  }}
+                  onImageClick={() => {
+                    onImageClick(item);
+                  }}
+                  className={`cursor-pointer transition-colors ${selectList.includes(index) ? 'bg-blue-50' : ''}`}
+                  multiple={multipleSelection}
+                />
+              ))
+            ) : (
+              <div className='text-muted-foreground flex flex-col items-center justify-center py-10 text-center'>
+                <Search className='mb-2 h-10 w-10 opacity-20' />
+                <p>검색 결과가 없습니다</p>
+                <p className='text-sm'>필터를 조정하거나 초기화해 보세요</p>
+              </div>
+            )}
+          </CardContent>
+        </Card>
+      </div>
+      <div className='pt-2'>
+        {/* 페이지네이션 */}
+        <Pagination>
+          <PaginationContent>
+            {/* 이전 */}
+            <PaginationItem>
+              <PaginationPrevious
+                onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                className={currentPage === 1 ? 'pointer-events-none opacity-50' : 'cursor-pointer'}
+                href='#'
               />
-            ))
-          ) : (
-            <div className='text-muted-foreground flex flex-col items-center justify-center py-10 text-center'>
-              <Search className='mb-2 h-10 w-10 opacity-20' />
-              <p>검색 결과가 없습니다</p>
-              <p className='text-sm'>필터를 조정하거나 초기화해 보세요</p>
-            </div>
-          )}
-        </CardContent>
-      </Card>
+            </PaginationItem>
+
+            {/* 첫번째 페이지 */}
+            {currentPage > 2 && (
+              <PaginationItem>
+                <PaginationLink onClick={() => setCurrentPage(1)} href='#'>
+                  1
+                </PaginationLink>
+              </PaginationItem>
+            )}
+
+            {/* 생략 */}
+            {currentPage > 3 && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+
+            {/* 이전페이지 */}
+            {currentPage > 1 && (
+              <PaginationItem>
+                <PaginationLink onClick={() => setCurrentPage(currentPage - 1)} href='#'>
+                  {currentPage - 1}
+                </PaginationLink>
+              </PaginationItem>
+            )}
+
+            {/* 현재 페이지 */}
+            <PaginationItem>
+              <PaginationLink href='#' isActive>
+                {currentPage}
+              </PaginationLink>
+            </PaginationItem>
+
+            {/* 다음페이지 */}
+            {currentPage < totalPages && (
+              <PaginationItem>
+                <PaginationLink onClick={() => setCurrentPage(currentPage + 1)} href='#'>
+                  {currentPage + 1}
+                </PaginationLink>
+              </PaginationItem>
+            )}
+
+            {/* 생략 */}
+            {currentPage < totalPages - 2 && (
+              <PaginationItem>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )}
+
+            {/* 마지막페이지 */}
+            {currentPage < totalPages - 1 && (
+              <PaginationItem>
+                <PaginationLink onClick={() => setCurrentPage(totalPages)} href='#'>
+                  {totalPages}
+                </PaginationLink>
+              </PaginationItem>
+            )}
+
+            {/* 다음 */}
+            <PaginationItem>
+              <PaginationNext
+                onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+                className={
+                  currentPage === totalPages ? 'pointer-events-none opacity-50' : 'cursor-pointer'
+                }
+                href='#'
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
 
       {/* Create 플로팅 버튼 */}
       <Link
         to='/item/new'
-        className='fixed right-10 bottom-10 z-50 flex cursor-pointer items-center justify-center rounded-full bg-blue-400 p-3.5 text-white shadow-lg transition-colors hover:bg-blue-500 hover:shadow-sm'
+        className='fixed right-10 bottom-17 z-50 flex cursor-pointer items-center justify-center rounded-full bg-blue-400 p-3.5 text-white shadow-lg transition-colors hover:bg-blue-500 hover:shadow-sm'
+        aria-label='Create'
       >
         <LuPlus size={24} />
       </Link>
